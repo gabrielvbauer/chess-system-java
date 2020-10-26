3005ecd922fc9d1604f27ab6e7caaee9771da82c
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2020-10-26 09:41:57 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
   public static void main(String[] args) {
-
+    Position position = new Position(5, 4);
+    System.out.println(position);
   }
 }
